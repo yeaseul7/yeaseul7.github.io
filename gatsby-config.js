37b57fad16c660da9f3b682badd706d544f8a431
seulseul_0123 <cutie32769@gmail.com>
2023-12-17 15:41:43 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Eslint_00's blog`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
